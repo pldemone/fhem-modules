f0f08ed84a3993ad75fa0ac47e5a051ae260589d
Mirko Lindner <demon@pro-linux.de>
2021-04-23 15:48:46 +0200

    play.sh: Presence check added
    Option parameter extended. It is possible to pass a FHEM PRESENCE device to the script, which must be present. Otherwise the output will be suppressed.
    
    Signed-off-by: Mirko Lindner <demon@pro-linux.de>

--- tools/play.sh
+++ tools/play.sh
@@ -9,12 +9,25 @@
 #	attr myTTS TTS_MplayerCall /opt/fhem_tools/play.sh {file} "{options}"
 #
 # Call from FHEM:
-#	set System.tts tts This is a test
-#	set System.tts tts [office] This is a test
-#	set System.tts tts [office,living] This is a test
-#
+#	set myTTS tts [ROOMS%FHEM_PRESENCE_DEVICE] MESSAGE
+
+# Examples:
+# 	Play in default room:
+#		set System.tts tts This is a test
+# 	Play in a specific room:
+#		set System.tts tts [office] This is a test
+# 	Play in multiple rooms
+#		set System.tts tts [office,living] This is a test
+# 	Play in specific room ony if system is present:
+#		set System.tts tts [office%%MyMobile] This is a test
+# 	Play in specific room ony if one of the systems is present:
+#		set System.tts tts [office%%MyMobile||My2Mobile] This is a test
+# 	Play in specific room ony if BOTH of the systems are present:
+#		set System.tts tts [office%%MyMobile&&My2Mobile] This is a test
+# 	Play in multiple rooms if system is present:
+#		set System.tts tts [office%%MyMobile,living%%MyMobile] This is a test
 # Usage outside from FHEM:
-#	./play.sh filename.mp3 office
+#	./play.sh filename.mp3 "office%%MyMobile"
 #
 # Copyright: Copyright (c) 2021, Mirko Lindner <demon@pro-linux.de>
 # License:   GPL-2
@@ -32,15 +45,15 @@ defhost="office"
 # the script searches via DNS for the IP address and the avahi port
 declare -A SYSTEMS=( ["office"]="192.168.0.130:7000" ["child"]="192.168.0.131:7000" ["hifi"]="VSX-527.local")
 
-# Nothing to change
+### Nothing to change
+#############################################################
 
 # Check tools
 which ffmpeg >/dev/null 2>&1 || { echo "ffmpeg required but not found. Aborting." >&2; exit 1; }
 which raop_play >/dev/null 2>&1 || { echo "ffmpeg required but not found. Aborting." >&2; exit 1; }
 which avahi-browse >/dev/null 2>&1 || { echo "avahi-browse required but not found. Aborting." >&2; exit 1; }
+if [ ! -e /opt/fhem/fhem.pl ]; then echo "/opt/fhem/fhem.pl required but not found. Aborting."; fi
 
-### Nothing to change
-#############################################################
 infile=$1
 playhost=$2
 
@@ -51,6 +64,7 @@ if [ "$infile" == "" ]; then
 	echo Exit
 	exit 1
 fi
+clear
 
 if [ "$playhost" == "" ]; then
 	playhost=$defhost;
@@ -87,29 +101,56 @@ if [ ! -e "${dirname}/${filename}.wav" ]; then
 fi
 
 for element in "${HOSTS[@]}"; do
-	if [ "${SYSTEMS[$element]}" != "" ]; then
+	IFS='%% ' read -r -a OPTIONS <<< "$element"
+	host=${OPTIONS[0]}
+	if  [ "$host" == "" ]; then continue; fi
+	fflag=0
+
+	# Check the presence tag
+	if [ "${OPTIONS[2]}" != "" ]; then
+		IFS='&& ' read -r -a PRESENCE <<< "${OPTIONS[2]}"
+		for presence in "${PRESENCE[@]}"; do
+			if  [ "$presence" == "" ]; then continue; fi
+			fflag=1
+			IFS='|| ' read -r -a ORCHECK <<< "${presence}"
+			for orcheck in "${ORCHECK[@]}"; do
+				syspres=`perl /opt/fhem/fhem.pl 7072 "LIST $orcheck state" | grep present -c`
+				if [[ $syspres -gt 0 ]]; then
+					fflag=2
+					break
+				fi
+			done
+
+			if [[ $fflag -eq 1 ]]; then break; fi
+		done
+	fi
+
+	# Presence - device(s) available
+	if [[ $fflag -eq 1 ]]; then continue; fi
+
+	if [ "${SYSTEMS[${host}]}" != "" ]; then
 	# Pre-defined device found
-		hostname=$(echo ${SYSTEMS[$element]} | cut -d : -f 1)
-		port=$(echo ${SYSTEMS[$element]} | cut -d : -f 2)
+		hostname=$(echo ${SYSTEMS[${host}]} | cut -d : -f 1)
+		port=$(echo ${SYSTEMS[${host}]} | cut -d : -f 2)
 		if [ "$port" == "$hostname" ]; then
 			# Port not available. Get from avahi...
 			port=`avahi-browse -rpkt _raop._tcp | grep $hostname | cut  -d ";" -f 9`
 		fi
 		if [ "$port" != "" ]  && [ "$hostname" != "" ]; then
-			echo "Play ${filename}.wav on ${hostname}:${port}"
-			raop_play $hostname -w 200 -v 50 -p $port "${dirname}/${filename}.wav"
+			echo "play ${filename}.wav on ${hostname}:${port}"
+			raop_play $hostname -w 200 -v 50 -p $port -d 0 "${dirname}/${filename}.wav"
 		fi
 	else
 	# Device not defined... Check DNS and avahi
-		ipaddr=`getent hosts $element | awk '{ print $1 }'`
+		ipaddr=`getent hosts ${host} | awk '{ print $1 }'`
 		if [ "$ipaddr" != "" ]; then
 			port=`avahi-browse -rpkt _raop._tcp | grep $ipaddr | cut  -d ";" -f 9 | head -1`
 			if [ "$port" != "" ]; then
-				echo "Play ${filename}.wav on $ipaddr:${port} (DNS)"
-				raop_play $ipaddr -w 200 -v 50 -p $port "${dirname}/${filename}.wav"
+				echo "play ${filename}.wav on $ipaddr:${port} (DNS)"
+				raop_play $ipaddr -w 200 -v 50 -p $port -d 0 "${dirname}/${filename}.wav" 
 			fi
 		else
-			echo $element not found - neither as definistion in script nor as DNS entry
+			echo ${host} not found - neither as definistion in script nor as DNS entry
 			exit 1
 		fi
 	fi
